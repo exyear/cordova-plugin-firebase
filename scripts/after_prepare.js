0df445cdb094b4cdbdb960413c8446058647ee34
exyear <chaosdevil112@gmail.com>
2017-02-16 12:40:46 +0800

    Update after_prepare.js

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -51,16 +51,16 @@ if (directoryExists("platforms/ios")) {
 }
 
 if (directoryExists("platforms/android")) {
-  var paths = ["google-services.json", "platforms/android/assets/www/google-services.json"];
+  var paths = ["google-services.json", "assets/www/google-services.json"];
 
   for (var i = 0; i < paths.length; i++) {
     if (fileExists(paths[i])) {
       try {
         var contents = fs.readFileSync(paths[i]).toString();
-        fs.writeFileSync("platforms/android/google-services.json", contents);
+        fs.writeFileSync("assets/www/google-services.json", contents);
 
         var json = JSON.parse(contents);
-        var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
+        var strings = fs.readFileSync("res/values/strings.xml").toString();
 
         // strip non-default value
         strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)</string>', "i"), '')
@@ -77,7 +77,7 @@ if (directoryExists("platforms/android")) {
         // replace the default value
         strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
 
-        fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
+        fs.writeFileSync("res/values/strings.xml", strings);
       } catch(err) {
         process.stdout.write(err);
       }
